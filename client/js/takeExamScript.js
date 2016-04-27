8923fdf0ec6cd460c0f9ec8fca66cba1bccc3312
RListerberg <Robin_listerberg@hotmail.com>
2016-04-27 12:57:47 +0200

    takeExam, lagt till sidräknare och frågeslider

--- client/js/takeExamScript.js
+++ client/js/takeExamScript.js
@@ -1,37 +1,54 @@
 /**
  * Created by robin on 2016-04-26.
  */
-// $(document).ready(function () {
-//     $(".questions div").each(function(e){
-//         if(e != 0)
-//             $(this).hide();
-//     });
-//
-//     $(".nextBut").click(function () {
-//         if($(".questions div:visible").next().length != 0)
-//             $(".questions div:visible").next().show().prev().hide();
-//         else {
-//             $(".questions div:visible").hide();
-//             $(".questions div:first").show();
-//         }
-//         return false;
-//     });
-//     $(".prevBut").click(function () {
-//         if($(".questions div:visible").prev().length != 0)
-//             $(".questions div:visible").prev().show().next().hide();
-//         else{
-//             $(".questions div:visible").hide();
-//             $(".questions div:last").show();
-//         }
-//         return false
-//     });
-// });
+$(document).ready(function () {
 
-    $(document).ready(function(){
+   var maxForms = $("div .ExamForm").length;
+   var count = 1;
 
-        $('.ExamForm').hide().first().show();
-        
+   var pageMax = document.getElementById("max");
+   var pageCount = document.getElementById("count");
 
+    pageCount.innerHTML = count;
+    pageMax.innerHTML = maxForms;
+
+    $(".examForms .ExamForm").each(function(e){
+        if(e != 0)
+            $(this).hide();
     });
+
+    $(".nextBut").click(function () {
+
+        if(count < maxForms){
+            count++;
+            pageCount.innerHTML = count;
+        }
+        if($(".examForms .ExamForm:visible").next().length != 0)
+            $(".examForms .ExamForm:visible").next().show().prev().hide();
+        else {
+            $(".examForms .ExamForm:visible").hide();
+            $(".examForms .ExamForm:first").show();
+            count = 1;
+            pageCount.innerHTML = count;
+        }
+        return false;
+    });
+
+    $(".prevBut").click(function () {
+        if(count > 1){
+            count--;
+            pageCount.innerHTML = count;
+        }
+        if($(".examForms .ExamForm:visible").prev().length != 0)
+            $(".examForms .ExamForm:visible").prev().show().next().hide();
+        else{
+            $(".examForms .ExamForm:visible").hide();
+            $(".examForms .ExamForm:last").show();
+            count = maxForms;
+            pageCount.innerHTML = count;
+        }
+        return false
+    });
+});
    
   
