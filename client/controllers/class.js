63e565f953df9153e434785bd8b19ca6c29662a9
RListerberg <Robin_listerberg@hotmail.com>
2016-05-02 10:41:46 +0200

    Class Användare uppdaterad

--- client/controllers/class.js
+++ client/controllers/class.js
@@ -2,6 +2,13 @@
  * Created by Jonas on 2016-04-25.
  */
 
-myApp.controller('classCtrl', function ($scope, $http) {
+myApp.controller('makeSchoolClassCtrl', ['$scope', 'userService', 'ClassManager', function($scope, userService, ClassManager){
 
-});
+    $scope.saveSchoolClass = function () {
+        ClassManager.addClass($scope.class, function (data) {
+            $scope.class = data;
+            $scope.getAllClasses()
+        })
+    };
+
+}]);
