d4e9a47b477f1c017c068cf8a120363878ee8af5
RListerberg <Robin_listerberg@hotmail.com>
2016-05-04 13:07:33 +0200

    Class controllers, manage_class ser inlagda användare

--- client/controllers/class.js
+++ client/controllers/class.js
@@ -1,41 +1,51 @@
 /**
- * Created by Jonas on 2016-04-25.
+ * Created by Robin on 2016-04-25.
  */
 
-myApp.controller('classCtrl',['$scope', 'userService', 'StudentClassManager', function($scope, userService, StudentClassManager){
-    
-    
-    $scope.studentClasses = [];
+myApp.controller('classCtrl',['$scope', 'userService', 'StudentClassManager','UserManager', function($scope, userService, StudentClassManager, UserManager){
 
+    $scope.studentClass = "";
+    $scope.studentClasses = [];
+    $scope.users = [];
+
+    $scope.getAllUsers = function () {
+        UserManager.getAllUsers(function (data) {
+            $scope.users = data;
+        })
+    };
+    
     $scope.getAllStudentClasses = function () {
         StudentClassManager.getAllStudentClasses (function (data) {
             $scope.studentClasses = data;
         })
     };
-
+    // $scope.selectClass = function (data) {
+    //     StudentClassManager.getStudentClass(data._id, function (data) {
+    //         $scope.studentClass = data;
+    //     })
+    // };
+    
     $scope.loadClass = function (id) {
-        StudentClassManager.getStudentClass(id, function (data) {
+        StudentClassManager.getStudentClass(id._id, function (data) {
             $scope.studentClass = data;
         })
     };
-
+    
+    $scope.saveUser = function () {
+      UserManager.addUser($scope.user, function (data) {
+            $scope.user = data;
+            $scope.getAllUsers();
+        })  
+    };
+    
     $scope.saveSchoolClass = function () {
         StudentClassManager.addStudentClass($scope.class, function (data) {
-            console.log($scope.studentClasses);
             $scope.class = data;
             $scope.getAllStudentClasses();
         })
     };
-    $scope.selectedClass = function () {
-        
-    };
-    
-    $scope.deleteSelectedClass = function () {
-        StudentClassManager.deleteClass($scope.class, function () {
-
-        })  
-    };
     
     $scope.getAllStudentClasses();
+    $scope.getAllUsers();
 }]);
 
